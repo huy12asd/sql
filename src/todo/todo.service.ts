abe7b88b0a8b0133c9297ba98850886d06cf841a
huy12asd <legiahuy124578@gmail.com>
2024-04-08 22:44:02 +0700

    todo

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,44 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Todo } from './todo.entity';
+  import { Injectable } from '@nestjs/common';
+  import { InjectRepository } from '@nestjs/typeorm';
+  import { Repository } from 'typeorm';
+  import { Todo } from './todo.entity';
 
-@Injectable()
-export class TodoService {
-  constructor(
-    @InjectRepository(Todo)
-    private todoRepository: Repository<Todo>,
-  ) {}
+  @Injectable()
+  export class TodoService {
+    constructor(
+      @InjectRepository(Todo)
+      private todoRepository: Repository<Todo>,
+    ) {}
 
-  async findAll(): Promise<Todo[]> {
-    return this.todoRepository.find();
-  }
-
-  async create(todoData: Partial<Todo>): Promise<Todo> {
-    const todo = new Todo();
-    todo.todo = todoData.todo;
-    todo.done = todoData.done || false;
-    return this.todoRepository.save(todo);
-  }
-
-  async update(id: number, todoData: Partial<Todo>): Promise<Todo> {
-    const todo = await this.todoRepository.findOne({ where: { id } });
-    if (!todo) {
-      throw new Error('Todo not found');
+    async findAll(): Promise<Todo[]> {
+      return this.todoRepository.find();
     }
-    return this.todoRepository.save({ ...todo, ...todoData });
-  }
 
-  async remove(id: number): Promise<void> {
-    await this.todoRepository.delete(id);
-  }
-
-  async toggleEdit(id: number): Promise<Todo> {
-    const todo = await this.todoRepository.findOne({ where: { id } });
-    if (!todo) {
-      throw new Error('Todo not found');
+    async create(todoData: Partial<Todo>): Promise<Todo> {
+      const todo = new Todo();
+      todo.id=todoData.id;
+      todo.todo = todoData.todo;
+      todo.done = false;
+      return this.todoRepository.save(todo);//ko tao id co the tao them , khi tao bo id o database
     }
-    todo.done = !todo.done; // Chuyển đổi trạng thái chỉnh sửa
-    return this.todoRepository.save(todo);
+
+    async update(id: number, todoData: Partial<Todo>): Promise<Todo> {
+      const todo = await this.todoRepository.findOne({ where: { id } });
+      if (!todo) {
+        throw new Error('Todo not found');
+      }
+      return this.todoRepository.save({ ...todo, ...todoData });
+    }
+
+    async remove(id: number): Promise<void> {
+      await this.todoRepository.delete(id);
+    }
+
+    
   }
-}
